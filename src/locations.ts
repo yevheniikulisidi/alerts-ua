5040a7e29e6a9ce206adc9c271d801bd0bcaa091
Yevhenii Kulisidi <109991141+yevheniikulisidi@users.noreply.github.com>
2023-05-27 19:36:25 +0300

    Added methods with locations

---
+++ src/locations.ts
@@ -0,0 +1,112 @@
+import { Location } from './types';
+
+export const locations: Location[] = [
+  {
+    uid: '3',
+    location_title: 'Хмельницька область',
+  },
+  {
+    uid: '4',
+    location_title: 'Вінницька область',
+  },
+  {
+    uid: '5',
+    location_title: 'Рівненська область',
+  },
+  {
+    uid: '8',
+    location_title: 'Волинська область',
+  },
+  {
+    uid: '9',
+    location_title: 'Дніпропетровська область',
+  },
+  {
+    uid: '10',
+    location_title: 'Житомирська область',
+  },
+  {
+    uid: '11',
+    location_title: 'Закарпатська область',
+  },
+  {
+    uid: '12',
+    location_title: 'Запорізька область',
+  },
+  {
+    uid: '13',
+    location_title: 'Івано-Франківська область',
+  },
+  {
+    uid: '14',
+    location_title: 'Київська область',
+  },
+  {
+    uid: '15',
+    location_title: 'Кіровоградська область',
+  },
+  {
+    uid: '16',
+    location_title: 'Луганська область',
+  },
+  {
+    uid: '17',
+    location_title: 'Миколаївська область',
+  },
+  {
+    uid: '18',
+    location_title: 'Одеська область',
+  },
+  {
+    uid: '19',
+    location_title: 'Полтавська область',
+  },
+  {
+    uid: '20',
+    location_title: 'Сумська область',
+  },
+  {
+    uid: '21',
+    location_title: 'Тернопільська область',
+  },
+  {
+    uid: '22',
+    location_title: 'Харківська область',
+  },
+  {
+    uid: '23',
+    location_title: 'Херсонська область',
+  },
+  {
+    uid: '24',
+    location_title: 'Черкаська область',
+  },
+  {
+    uid: '25',
+    location_title: 'Чернігівська область',
+  },
+  {
+    uid: '26',
+    location_title: 'Чернівецька область',
+  },
+  {
+    uid: '27',
+    location_title: 'Львівська область',
+  },
+  {
+    uid: '28',
+    location_title: 'Донецька область',
+  },
+  {
+    uid: '29',
+    location_title: 'Автономна Республіка Крим',
+  },
+  {
+    uid: '30',
+    location_title: 'м. Севастополь',
+  },
+  {
+    uid: '31',
+    location_title: 'м. Київ',
+  },
+];
